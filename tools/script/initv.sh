0845af3e39ff54e1291c6699c7a732428c6387e6
skywind3000 <skywind3000@163.com>
2018-10-22 00:32:48 +0800

    first initv.sh

--- tools/script/initv.sh
+++ tools/script/initv.sh
@@ -1,3 +1,54 @@
 #! /bin/sh
 
+CMD="$1"
+INITD="$2"
+
+start() {
+	if [ -d "$INITD" ]; then
+		echo "Launching initialization scripts"
+		for f in $INITD/S*; do
+			# echo "file: $f"
+			[ -x "$f" ] && "$f" start
+		done
+	else
+		echo "error: %s directory not found" 1>&2
+		exit 1
+	fi
+}
+
+stop() {
+	if [ -d "$INITD" ]; then
+		echo "Launching termination scripts"
+		for f in $INITD/K*; do
+			[ -x "$f" ] && "$f" stop
+		done
+	else
+		echo "error: %s directory not found" 1>&2
+		exit 1
+	fi
+}
+
+restart() {
+	stop "$1"
+	start "$1"
+}
+
+_term() {
+	trap "" TERM INT
+	stop
+	exit 0
+}
+
+service() {
+	start
+	trap _term TERM INT
+	sleep infinity
+	stop
+	exit 0
+}
+
+INITD=/home/skywind/tmp/init.d
+
+service
+
 
