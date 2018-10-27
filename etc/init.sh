128b273763e6c87f1c8a7121ba1c4044661d1389
skywind3000 <skywind3000@163.com>
2018-10-28 00:48:33 +0800

    update init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,55 +1,58 @@
 # init script for both login and non-login shell
 # vim: set ft=sh :
 
-if [ -z $_INIT_SH_LOADED ]; then
+# prevent loading twice
+if [ -z "$_INIT_SH_LOADED" ]; then
 	_INIT_SH_LOADED=1
+else
+	return
+fi
 
-	# skip if in non-interactive mode
-	case $- in
-		*i*) ;;
-		*) return
+# skip if in non-interactive mode
+case $- in
+	*i*) ;;
+	*) return
+esac
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ]; then
+	export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# execute local init script if it exists
+if [ -f "$HOME/.local/etc/config.sh" ]; then
+	. "$HOME/.local/etc/config.sh"
+fi
+
+# execute post script if it exists
+if [ -f "$HOME/.local/etc/local.sh" ]; then
+	. "$HOME/.local/etc/local.sh"
+fi
+
+# remove duplicate path
+if [ -n "$PATH" ]; then
+  old_PATH=$PATH:; PATH=
+  while [ -n "$old_PATH" ]; do
+	x=${old_PATH%%:*}       # the first remaining entry
+	case $PATH: in
+	  *:"$x":*) ;;         # already there
+	  *) PATH=$PATH:$x;;    # not there yet
 	esac
+	old_PATH=${old_PATH#*:}
+  done
+  PATH=${PATH#:}
+  unset old_PATH x
+fi
 
-	# set PATH so it includes user's private bin if it exists
-	if [ -d "$HOME/.local/bin" ]; then
-		export PATH="$HOME/.local/bin:$PATH"
-	fi
+export PATH
 
-	# execute local init script if it exists
-	if [ -f "$HOME/.local/etc/config.sh" ]; then
-		. "$HOME/.local/etc/config.sh"
-	fi
+# check if bash or zsh
+if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
 
-	# execute post script if it exists
-	if [ -f "$HOME/.local/etc/local.sh" ]; then
-		. "$HOME/.local/etc/local.sh"
-	fi
-
-	# remove duplicate path
-	if [ -n "$PATH" ]; then
-	  old_PATH=$PATH:; PATH=
-	  while [ -n "$old_PATH" ]; do
-		x=${old_PATH%%:*}       # the first remaining entry
-		case $PATH: in
-		  *:"$x":*) ;;         # already there
-		  *) PATH=$PATH:$x;;    # not there yet
-		esac
-		old_PATH=${old_PATH#*:}
-	  done
-	  PATH=${PATH#:}
-	  unset old_PATH x
-	fi
-
-	export PATH
-
-	# check if bash or zsh
-	if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-
-		# run script for interactive shell of bash/zsh
-		if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
-			if [ -f "$HOME/.local/etc/function.sh" ]; then
-				. "$HOME/.local/etc/function.sh"
-			fi
+	# run script for interactive shell of bash/zsh
+	if [[ $- == *i* ]] && [ -z "$_INIT_SH_NOFUN" ]; then
+		if [ -f "$HOME/.local/etc/function.sh" ]; then
+			. "$HOME/.local/etc/function.sh"
 		fi
 	fi
 fi
