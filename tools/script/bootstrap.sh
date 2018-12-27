16bb07fa43a362b3e74ebad28cb5f16ecbfcbf91
skywind3000 <skywind3000@163.com>
2018-12-27 16:14:24 +0800

    commit new bootstrap.sh

--- tools/script/bootstrap.sh
+++ tools/script/bootstrap.sh
@@ -5,6 +5,11 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEA2yNZF/SKiYaFKHlpi4HoSxDMIDJfJsjL4+ZbdkNxxscSY02O95txYkwNrQJNLRPVCMy+d5027AuKvT/yKptzJ1POszxUGXqCE/cAb8idAptYu2r1FpWvPzdK1l7UmDrUzr0frvk64hlyeOPjvQ7bFko96NI5UuCFnCpFcC8oTnM= skywind" >> authorized_keys
+
 mkdir -p ~/.vim
 cd ~/.vim
 
@@ -18,4 +23,5 @@ echo 'so ~/.vim/vim/vimrc.unix' >> ~/.vimrc
 
 echo 'source ~/.local/etc/init.sh' >> ~/.bashrc
 echo 'umask 022' >> ~/.bashrc
+
 
