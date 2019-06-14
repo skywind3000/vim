31cbba3263c381e02dd514028660a0057190ca47
skywind3000 <skywind3000@163.com>
2019-06-14 17:04:01 +0800

    complete translation

--- lib/translator.py
+++ lib/translator.py
@@ -37,10 +37,10 @@ class BasicTranslator(object):
         if self._agent:
             header['User-Agent'] = self._agent
         argv['headers'] = header
-        timeout = self._options.get('timeout', 10)
+        timeout = self._options.get('timeout', 7)
         proxy = self._options.get('proxy', None)
         if timeout:
-            argv['timeout'] = timeout
+            argv['timeout'] = float(timeout)
         if proxy:
             proxies = {'http': proxy, 'https': proxy}
             argv['proxies'] = proxies
@@ -50,33 +50,17 @@ class BasicTranslator(object):
         else:
             if data is not None:
                 argv['data'] = data
-        try:
-            if not post:
-                r = self._session.get(url, **argv)
-            else:
-                r = self._session.post(url, **argv)
-        except requests.exceptions.ConnectionError:
-            r = None
-        except requests.exceptions.RetryError as e:
-            r = requests.Response()
-            r.status_code = -1
-            r.text = 'RetryError'
-            r.error = e
-        except requests.exceptions.BaseHTTPError as e:
-            r = requests.Response()
-            r.status_code = -2
-            r.text = 'BaseHTTPError'
-            r.error = e
-        except requests.exceptions.HTTPError as e:
-            r = requests.Response()
-            r.status_code = -3
-            r.text = 'HTTPError'
-            r.error = e
-        except requests.exceptions.RequestException as e:
-            r = requests.Response()
-            r.status_code = -4
-            r.error = e
+        if not post:
+            r = self._session.get(url, **argv)
+        else:
+            r = self._session.post(url, **argv)
         return r
+
+    def http_get (self, url, data = None, header = None):
+        return self.request(url, data, False, header)
+
+    def http_post (self, url, data = None, header = None):
+        return self.request(url, data, True, header)
 
     def url_unquote (self, text, plus = True):
         if sys.version_info[0] < 3:
@@ -100,17 +84,30 @@ class BasicTranslator(object):
             return urllib.parse.quote_plus(text)
         return urllib.parse.quote(text)
 
-    # src: 源语言，None 为自动识别
-    # dst: 目标语言
+    # sl: 源语言，auto 为自动识别
+    # tl: 目标语言
     # text: 待翻译文字
-    def translate (self, src, dst, text):
+    def translate (self, sl, tl, text):
         res = {}
-        res['src'] = src
-        res['dst'] = dst
+        res['sl'] = sl
+        res['tl'] = sl
         res['text'] = text
         res['translation'] = None
         res['success'] = False
         return res
+
+    # 是否是英文
+    def check_english (self, text):
+        for ch in text:
+            if ord(ch) >= 128:
+                return False
+        return True
+
+    # 猜测语言
+    def guess_language (self, text):
+        if self.check_english(text):
+            return ('en-US', 'zh-CN')
+        return ('zh-CN', 'en-US')
     
 
 #----------------------------------------------------------------------
@@ -123,6 +120,25 @@ class GoogleTranslator (BasicTranslator):
         self._agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0)'
         self._agent += ' Gecko/20100101 Firefox/59.0'
 
+    def get_url (self, sl, tl, qry):
+        http_host = self._options.get('host', 'translate.googleapis.com')
+        http_host = 'translate.google.cn'
+        qry = self.url_quote(qry)
+        url = 'https://{}/translate_a/single?client=gtx&sl={}&tl={}&dt=at&dt=bd&dt=ex&' \
+              'dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&q={}'.format(
+                      http_host, sl, tl, qry)
+        return url
+
+    def translate (self, sl, tl, text):
+        if (sl is None or sl == 'auto') and (tl is None or tl == 'auto'):
+            sl, tl = self.guess_language(text)
+        url = self.get_url(sl, tl, text)
+        r = self.http_get(url)
+        obj = r.json()
+        import pprint
+        pprint.pprint(obj)
+        return 0
+
 
 #----------------------------------------------------------------------
 # testing suit
@@ -133,4 +149,11 @@ if __name__ == '__main__':
         r = bt.request("http://www.baidu.com")
         print(r.text)
         return 0
-    test1()
+    def test2():
+        gt = GoogleTranslator()
+        print(gt.translate('auto', 'auto', 'Hello, World !!'))
+        return 0
+    test2()
+
+
+
