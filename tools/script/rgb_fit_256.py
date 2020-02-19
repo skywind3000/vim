79cd254c2f03aa5edd074d716126e5f4dc526cda
skywind3000 <skywind3000@163.com>
2020-02-20 00:17:28 +0800

    rgb fit 256

---
+++ tools/script/rgb_fit_256.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#======================================================================
+#
+# ColorFit256.py - 
+#
+# Created by skywind on 2020/02/20
+# Last Modified: 2020/02/20 00:12:05
+#
+#======================================================================
+from __future__ import print_function, unicode_literals
+import sys
+import os
+
