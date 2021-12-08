565ed3d976f5f591b88069884b9d432200cb4d65
skywind3000 <skywind3000@163.com>
2021-12-08 21:26:27 +0800

    update linux brew init script

---
+++ etc/linuxbrew.sh
@@ -0,0 +1,6 @@
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+
