db2f4b8bcb2c261f25b50ef8ecb4ca73dc1d1540
skywind3000 <skywind3000@163.com>
2021-12-08 21:30:43 +0800

    update linuxbrew.sh

--- etc/linuxbrew.sh
+++ etc/linuxbrew.sh
@@ -1,3 +1,4 @@
+# see https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
 export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
