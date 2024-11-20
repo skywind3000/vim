21e41d46c59c2ba773184d4b505f50e57ded8526
skywind3000 <skywind3000@163.com>
2024-11-21 00:12:21 +0800

    update initz.sh

--- tools/share/system/initz.sh
+++ tools/share/system/initz.sh
@@ -76,6 +76,7 @@ initz_execute() {
 		for f in $INITZ_HOME/E*; do
 			[ -x "$f" ] && "$f"
 		done
+		[ -e "$INITZ_HOME/rc.local" ] && . "$INITZ_HOME/rc.local"
 	else
 		echo "error: %s directory not found" 1>&2
 		exit 1
