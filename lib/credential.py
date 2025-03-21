741227ba7a2fea8c18fe37e44881575096498ce0
skywind3000 <skywind3000@163.com>
2025-03-21 16:43:21 +0800

    Add getopt function for parsing command line arguments in credential.py

--- lib/credential.py
+++ lib/credential.py
@@ -227,6 +227,34 @@ def profile_uuid():
 
 
 #----------------------------------------------------------------------
+# getopt: returns (options, args)
+#----------------------------------------------------------------------
+def getopt (self, argv):
+    args = []
+    options = {}
+    if argv is None:
+        argv = sys.argv[1:]
+    index = 0
+    count = len(argv)
+    while index < count:
+        arg = argv[index]
+        if arg != '':
+            head = arg[:1]
+            if head != '-':
+                break
+            if arg == '-':
+                break
+            name = arg.lstrip('-')
+            key, _, val = name.partition('=')
+            options[key.strip()] = val.strip()
+        index += 1
+    while index < count:
+        args.append(argv[index])
+        index += 1
+    return options, args
+
+
+#----------------------------------------------------------------------
 # samples for git credential standard input
 #----------------------------------------------------------------------
 SAMPLES = '''
