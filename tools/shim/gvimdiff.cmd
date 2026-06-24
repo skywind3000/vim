20946cea096ae52e958732a3bdc48dbdb5cf03a6
skywind3000 <skywind3000@163.com>
2026-06-24 22:09:02 +0800

    update gvimdiff.cmd

--- tools/shim/gvimdiff.cmd
+++ tools/shim/gvimdiff.cmd
@@ -2,9 +2,9 @@
 
 if "%2" == "" GOTO HELP
 
-call "C:\Program Files (x86)\Vim\vim92\gvim.exe" --remote-send "<ESC>:tabe %~f1<cr>:vert diffsplit %~f2<CR>"
+call "C:\Program Files (x86)\Vim\gvim.exe" --remote-send "<ESC>:tabe %~f1<cr>:vert diffsplit %~f2<CR>"
 REM call "C:\Program Files (x86)\Vim\vim92\gvim.exe" --remote-send ":call foreground()<CR>"
-call "C:\Program Files (x86)\Vim\vim92\gvim.exe" --cmd "call remote_foreground('GVIM') | quit!"
+call "C:\Program Files (x86)\Vim\gvim.exe" --cmd "call remote_foreground('GVIM') | quit!"
 
 GOTO :EXIT
 
